6a0d89a051b95bd00b3dd6e476f3c893a0cd008a
TatsukiNishimura <hbvcg00@gmail.com>
2024-11-07 16:58:09 +0900

    update paper model

--- paper_graph/models.py
+++ paper_graph/models.py
@@ -5,12 +5,12 @@ class Paper(models.Model):
     title = models.CharField(max_length=255)
     authors = models.CharField(max_length=255)
     publication_year = models.IntegerField()
-    abstract = models.TextField()
-    doi = models.CharField(max_length=100, unique=True)
+    abstract = models.TextField(blank=True)
+    doi = models.CharField(max_length=100, blank=True)
     citations = models.ManyToManyField(
         "self", symmetrical=False, related_name="cited_by", blank=True
     )
-    paper_url = models.URLField(default="")
+    paper_url = models.URLField(default="", blank=True)
 
     def __str__(self):
         return self.title
